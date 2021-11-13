90dc39a2e409443947460dcaf31a0d32b18b2442
CalebH <caleb.harmon2345@gmail.com>
2021-11-13 12:46:06 +0000

    Allowed display to work with a paramater passed

--- display.cpp
+++ display.cpp
@@ -1,20 +1,17 @@
 
 using namespace std;
 #include <iostream>
-#include <cellularAutomaton.h>
+#include "cellularAutomaton.h"
 
-int testArray[10] = {0,0,0,0,1,0,1,0,1,0};
+// int testArray[10] = {0,0,0,0,1,0,1,0,1,0};
 
-void display(){
+void display(int cellArray[]){
     for(int i = 0; i < 10; i++){
-        if(testArray[i] == 1){
+        if(cellArray[i] == 1){
             cout << "#";
         }else{
             cout << "-";
         }
     }
     cout << '\n';
-}
-int main(){
- display();
 }
--- tester.cpp
+++ tester.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "cellularAutomaton.h"
+#include "display.cpp"
 using namespace std;
 
 int main()
 {
+    int testArray[10] = {0,0,0,0,1,0,1,0,1,0};
+    display(testArray);
 
     return 0;
 }
