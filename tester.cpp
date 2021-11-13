6827984199932bdbec16bc684f10309ec7b643b1
davidtopping02 <71976336+davidtopping02@users.noreply.github.com>
2021-11-13 21:41:40 +0000

    bug fixes

--- tester.cpp
+++ tester.cpp
@@ -11,7 +11,7 @@ int main()
 
     //creating new rule set and setting it to rule 30
     RulesSet theRules;
-    theRules.setRulesFromBinary(toBinary(50));
+    theRules.setRulesFromBinary(toBinary(30));
 
     //creating a sample current line and initialising all values to 0
     int lineArray[81];
@@ -22,7 +22,7 @@ int main()
     }
 
     //setting middle array elemenet to 1
-    lineArray[81] = 1;
+    lineArray[41] = 1;
 
     newLine(theRules, lineArray, 40);
 
