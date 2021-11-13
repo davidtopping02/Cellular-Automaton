697493be6187cb257e29c2bccaa812f6ebd625ec
CalebH <caleb.harmon2345@gmail.com>
2021-11-13 12:54:09 +0000

    fixing conflict

--- tester.cpp
+++ tester.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include "cellularAutomaton.h"
-#include "display.cpp"
 using namespace std;
 
 int main()
 {
-    int testArray[10] = {0,0,0,0,1,0,1,0,1,0};
-    display(testArray);
-
     return 0;
 }
